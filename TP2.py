2f430e5e29df5f052d94d66ba7f182fc5b4444f6
SugarBoi23 <tuhienminh0@gmail.com>
2024-10-06 10:53:44 -0400

    Version finale

--- TP2.py
+++ TP2.py
@@ -1,14 +1,5 @@
 import csv
-import datetime
-try:
-    from _datetime import *
-    from _datetime import __doc__
-except ImportError:
-    from _pydatetime import *
-    from _pydatetime import __doc__
-
-__all__ = ("date", "datetime", "time", "timedelta", "timezone", "tzinfo",
-           "MINYEAR", "MAXYEAR", "UTC")
+from datetime import *
 
 """
 TP2 : Système de gestion de livres pour une bibliothèque
@@ -18,87 +9,83 @@ Numéro d'équipe :  05
 Noms et matricules : TU, Hien-Minh (2377236), ADAOURI, Aymene (2398803)
 """
 
-########################################################################################################## 
+##########################################################################################################
 # PARTIE 1 : Création du système de gestion et ajout de la collection actuelle
-########################################################################################################## 
+##########################################################################################################
 
-bibliotheque = {}
+with open('collection_bibliotheque.csv', newline='') as premiere_collection:
+    premiere_collection = csv.DictReader(premiere_collection)
+    bibliotheque = {}
 
-with open("collection_bibliotheque.csv", newline='') as dictionnaire:
-    dictionnaire = csv.reader(dictionnaire)
-    next(dictionnaire)
-    for row in dictionnaire:
-        bibliotheque[row[3]] = {"titre" : row[0],"auteur" : row[1],"date_publication" : row[2]}
+    for row in premiere_collection:
+        bibliotheque[row['cote_rangement']] = {"titre" : row["titre"], "auteur" : row["auteur"], "date_publication" : row["date_publication"]}
 
 print(f' \n Bibliotheque initiale : {bibliotheque} \n')
 
-########################################################################################################## 
+##########################################################################################################
 # PARTIE 2 : Ajout d'une nouvelle collection à la bibliothèque
 ##########################################################################################################
 
-with open("nouvelle_collection.csv", newline='') as new_collection:
-    new_collection = csv.reader(new_collection)
-    next(new_collection)
+with open('nouvelle_collection.csv', newline='') as nouvelle_collection:
+    nouvelle_collection = csv.DictReader(nouvelle_collection)
 
-    for new_row in new_collection:
-        titre_c, auteur_c, date_c, cle_c = new_row[0], new_row[1], new_row[2], new_row[3]
-        list_keys = tuple(bibliotheque.keys())[1:]
-        if not cle_c in list_keys:
-            print(f"Le livre {cle_c} ---- {titre_c} par {auteur_c} ---- a été ajouté avec succès")
-            bibliotheque[cle_c] = {"titre" : titre_c,"auteur" : auteur_c,"date_publication" : date_c}
+    for row in nouvelle_collection:
+        if row['cote_rangement'] not in bibliotheque.keys():
+            bibliotheque[row['cote_rangement']] = {"titre" : row["titre"], "auteur" : row["auteur"], "date_publication" : row["date_publication"]}
+            print(f"Le livre {row['cote_rangement']} ---- {row['titre']} par {row['auteur']} ---- a été ajouté avec succès")
         else:
-            print(f"Le livre {cle_c} ---- {titre_c} par {auteur_c} ---- est déjà présent dans la bibliothèque")
+            print(f"Le livre {row['cote_rangement']} ---- {row['titre']} par {row['auteur']} ---- est déjà présent dans la bibliothèque")
 
-########################################################################################################## 
+
+##########################################################################################################
 # PARTIE 3 : Modification de la cote de rangement d'une sélection de livres
-########################################################################################################## 
+##########################################################################################################
 
-for key in tuple(bibliotheque.keys()):
-    if bibliotheque[key]["auteur"] == "William Shakespeare":
-        bibliotheque["WS" + key[1:]] = bibliotheque.pop(key)
+for cote in tuple(bibliotheque.keys()):
+    if bibliotheque[cote]["auteur"] == "William Shakespeare":
+        bibliotheque["WS" + cote[1:]] = bibliotheque.pop(cote)
 
 print(f' \n Bibliotheque avec modifications de cote : {bibliotheque} \n')
 
-########################################################################################################## 
+##########################################################################################################
 # PARTIE 4 : Emprunts et retours de livres
-########################################################################################################## 
+##########################################################################################################
 
-emprunts_keys = []
+emprunts_cote = []
 
 with open("emprunts.csv", newline='') as emprunts:
-    emprunts = csv.reader(emprunts)
-    next(emprunts)
-    for key in bibliotheque:
-        bibliotheque[key]["emprunts"] = "disponible"
-        bibliotheque[key]["date_emprunt"] = "----------"
+    emprunts = csv.DictReader(emprunts)
+    for cote in bibliotheque.keys():
+        bibliotheque[cote]["emprunts"] = "disponible"
 
     for row in emprunts:
-        if row[0] in bibliotheque:
-            bibliotheque[row[0]]["emprunts"] = "emprunte"
-            bibliotheque[row[0]]["date_emprunt"] = row[1]
-            emprunts_keys.append(row[0])
+        if row['cote_rangement'] in bibliotheque.keys():
+            bibliotheque[row['cote_rangement']]["emprunts"] = "emprunté"
+            bibliotheque[row['cote_rangement']]["date_emprunt"] = row['date_emprunt']
+            emprunts_cote.append(row['cote_rangement'])
 
+print(f'\n Bibliotheque avec ajout des emprunts : {bibliotheque} \n')
 
-print(f' \n Bibliotheque avec ajout des emprunts : {bibliotheque} \n')
-
-########################################################################################################## 
-# PARTIE 5 : Livres en retard 
-########################################################################################################## 
+##########################################################################################################
+# PARTIE 5 : Livres en retard
+##########################################################################################################
 
 current_day = datetime.now().strftime("%Y-%m-%d").split("-")
 current_day = datetime(*map(int, current_day))
 
-for key in emprunts_keys:
-    if bibliotheque[key]["emprunts"] == "emprunte":
-        date_emprunt = datetime(*map(int, bibliotheque[key]["date_emprunt"].split("-")))
+for cote in emprunts_cote:
+    if bibliotheque[cote]["emprunts"] == "emprunté":
+        date_emprunt = datetime(*map(int, bibliotheque[cote]["date_emprunt"].split("-")))
         retour = (current_day - date_emprunt).days
         if 30 < retour <= 80:
-            bibliotheque[key]['frais_retard'] = f"{(retour - 30) * 2}$"
-            print(f"Le livre {key} est en retard : frais de {bibliotheque[key]['frais_retard']}")
+            bibliotheque[cote]['frais_retard'] = f"{(retour - 30) * 2}$"
+            print(f"Le livre {cote} est en retard : frais de {bibliotheque[cote]['frais_retard']}")
         elif 80 < retour < 365:
-            bibliotheque[key]["frais_retard"] = f"{100}$"
-            print(f"Le livre {key} est en retard : frais de {bibliotheque[key]['frais_retard']}")
+            bibliotheque[cote]["frais_retard"] = f"100$"
+            print(f"Le livre {cote} est en retard : frais de {bibliotheque[cote]['frais_retard']}")
         elif 365 <= retour:
-            bibliotheque[key]["livres_perdus"] = "perdu"
+            bibliotheque[cote]["livres_perdus"] = "perdu"
+            bibliotheque[cote]["frais_retard"] = f"100$"
+            print(f"Le livre {cote} est en retard : frais de {bibliotheque[cote]['frais_retard']}")
 
 print(f' \n Bibliotheque avec ajout des retards et frais : {bibliotheque} \n')
